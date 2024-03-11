b940888155f9eab277c3ef9e4125206f8b8689ff
Damir Zagretdinov <zagretdinov810@gmail.com>
2024-03-11 19:17:05 +0300

    aaa

---
+++ app.js
@@ -0,0 +1,18 @@
+
+/* can do
+let $ = 'test'
+let $num$ = 23
+let _ = 40
+let first_name = 'Elena'
+
+not can do
+let 45num = 23
+let mu-num= 56
+let const = 123
+*/
+
+//alert(firstName)
+// console.log(firstName)
+
+
+// console.log(num + 10)
